15d36fd87615fa5ef1ffa8f4bc38543b0ba4fdc7
LarisaLG <lgalisheva@gmail.com>
2022-12-21 22:58:26 +0000

    finish product size logic

--- bag/views.py
+++ bag/views.py
@@ -14,12 +14,24 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))  # got quantity from input
     redirect_url = request.POST.get('redirect_url')  # after item is added to bag redirect user to the same page
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):  # cjeck if element exists
-        bag[item_id] += quantity  # update quantity
+    if size:
+        if item_id in list(bag.keys()):  # check if element exists
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity  # update quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity  # create and add quantity to cart
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity  # create and add quantity to cart
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
