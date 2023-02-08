c7ea07f6b4e55e85142f603ca0c1da2f4a5946f4
LarisaLG <lgalisheva@gmail.com>
2023-02-08 17:55:38 +0000

    fix custom_storages.py

--- custom_storages.py
+++ custom_storages.py
@@ -4,7 +4,6 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
-git add . 
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
