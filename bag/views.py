30d3d4c5136f41e8d741b0d7471a10df608b075a
LarisaLG <lgalisheva@gmail.com>
2023-02-11 12:38:54 +0000

    add 404 page

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, reverse
+from django.shortcuts import HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -16,7 +17,8 @@ def add_to_bag(request, item_id):
     to the specified amount """
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))  # got quantity from input
-    redirect_url = request.POST.get('redirect_url')  # after item is added to bag redirect user to the same page
+    # after item is added to bag redirect user to the same page
+    redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
@@ -25,7 +27,8 @@ def add_to_bag(request, item_id):
     if size:
         if item_id in list(bag.keys()):  # check if element exists
             if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity  # update quantity
+                # update quantity
+                bag[item_id]['items_by_size'][size] += quantity
                 messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
             else:
                 bag[item_id]['items_by_size'][size] = quantity  # create and add quantity to cart
@@ -90,7 +93,8 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+            messages.success(request,
+                             (f'Removed size {size.upper()}{product.name} from your bag'))
         else:
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your bag')
